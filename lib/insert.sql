0b5ed99dc66da334b96fa15dceefd70ac1fdf0ca
Zacharia <ndunguzachary24@gmail.com>
2022-08-31 14:25:12 +0300

    commit tree 9af3c135e978be8c4bcf7cf0d273bb9c7e99f437

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Mr. Chocolate',  5,  'M', 'pink', 'DEF' , 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Rowdy', 7, 'F', 'pink', 'DEF' , 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Tabitha', 5, 'F', 'pink', 'DEF' , 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Sergeant Brown', 8, 'M', 'Red', 'DEF' , 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Melissa', 3, 'M', 'Brown', 'DEF' , 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Grinch', 5, 'M', 'pink', 'DEF' , 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Wendy', 5, 'F', 'pink', 'DEF' , 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( null, '6',  'F', 'green', 'DEF' , 1);
